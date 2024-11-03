80c0e12187ceee48a87500e8eebd5ba1753312c7
Memo Chou <memochou1993@gmail.com>
2024-11-03 16:23:10 +0800

    Update tests

--- lib/rules/regex.test.ts
+++ lib/rules/regex.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 import regex from './regex';
 
 describe('Rule "regex"', () => {
-  describe('using a valid expression', () => {
+  describe('set to a valid expression', () => {
     const validate = regex({ expression: /^[0-9]$/ });
 
     test('should pass with valid input', () => {
@@ -16,7 +16,7 @@ describe('Rule "regex"', () => {
     });
   });
 
-  describe('using a invalid expression', () => {
+  describe('set to an invalid expression', () => {
     test('should fail with invalid input', () => {
       // @ts-expect-error: Testing invalid input
       const validate = regex({ expression: 'foo' as unknown });
